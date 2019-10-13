7f5fcd168823fc3d8a57c9d2ae531c8069c5ca2b
P.M <vyfullspam50@gmail.com>
2019-10-13 18:50:03 +0300

    Create 01-ActionPoint3.cs

---
+++ 01-ActionPoint3.cs
@@ -0,0 +1,17 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _01_action_point
+{
+    class ActionPoint3
+    {
+        static void Main(string[] args)
+        {
+            Console.ReadLine()
+                .Split()
+                .ToList()
+                .ForEach(Console.WriteLine);
+        }
+    }
+}
