df295f5262b2dc479fdd9dac29f707829b58c149
P.M <vyfullspam50@gmail.com>
2019-10-15 18:44:47 +0300

    Add files via upload

---
+++ 06-speed-racing/Car.cs
@@ -0,0 +1,72 @@
+﻿
+namespace _06_speed_racing
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    public class CarsClass
+    {
+        private static List<Car> cars;
+
+        public List<Car> Cars { get => cars; private set => cars = value; }
+        private static Func<List<Car>, string, Car> getCar = (cars, model) =>
+        cars.Where(car => car.Model == model).FirstOrDefault();
+
+        public CarsClass()
+        {
+            Cars = new List<Car>();
+        }
+        public void NewCar(Car newCar)
+        {
+            if (Cars.Count == 0 || Cars.Any(car => car.Model != newCar.Model))
+            {
+                Cars.Add(newCar);
+            }
+        }
+
+        public void Drive(string model, double ammountOfKm)
+        {
+            if (getCar(Cars, model).FuelAmmount - getCar(Cars, model)
+                .FuelConsumptionPerKillometer * ammountOfKm >= 0 
+                && Cars.Any(car => car.Model == model))
+            {
+                Cars.Where(car => car.Model == model)
+                    .Select(car => car.FuelAmmount -=
+                    getCar(Cars, model).FuelConsumptionPerKillometer * ammountOfKm)
+                    .ToList();
+
+                Cars.Where(car => car.Model == model)
+                    .Select(car => car.DistanceTravelled 
+                    += ammountOfKm)
+                    .ToList();
+
+            }
+            else
+            {
+                System.Console.WriteLine($"Insufficient fuel for the drive");
+            }
+        }
+    }
+
+    public class Car
+    {
+        private string model;
+        private double fuelAmmount;
+        private double fuelConsumptionPerKillometer;
+        private double distanceTravelled;
+
+        public string Model { get => model; set => model = value; }
+        public double FuelAmmount { get => fuelAmmount; set => fuelAmmount = value; }
+        public double FuelConsumptionPerKillometer { get => fuelConsumptionPerKillometer; set => fuelConsumptionPerKillometer = value; }
+        public double DistanceTravelled { get => distanceTravelled; set => distanceTravelled = value; }
+
+        public Car(string model, double fuelAmmount, double fuelConsumption)
+        {
+            this.Model = model;
+            this.FuelAmmount = fuelAmmount;
+            this.FuelConsumptionPerKillometer = fuelConsumption;
+            this.DistanceTravelled = 0;
+        }
+
+    }
+}
---
+++ 06-speed-racing/StartUp.cs
@@ -0,0 +1,37 @@
+﻿
+namespace _06_speed_racing
+{
+using System;
+using System.Collections.Generic;
+    class StartUp
+    {
+
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            var carsList = new CarsClass();
+            for (int i = 0; i < n; i++)
+            {
+                var input = Console.ReadLine().Split();
+                string model = input[0];
+                double fuelAmmount = double.Parse(input[1]);
+                double fuelConsumption = double.Parse(input[2]);
+
+                carsList.NewCar(new Car(model, fuelAmmount, fuelConsumption));
+            }
+
+            string execute = "";
+            while((execute = Console.ReadLine()) != "End")
+            {
+                string[] arr = execute.Split();
+                string carModel = arr[1];
+                double ammountOfKm = double.Parse(arr[2]);
+                carsList.Drive(carModel, ammountOfKm);
+            }
+            foreach (var car in carsList.Cars)
+            {
+                Console.WriteLine($"{car.Model} {car.FuelAmmount:F2} {car.DistanceTravelled}");
+            }
+        }
+    }
+}
