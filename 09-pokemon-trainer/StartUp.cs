42e05a63d35b5201a629103096cac1ab133670e7
P.M <vyfullspam50@gmail.com>
2019-10-16 17:03:24 +0300

    Add files via upload
    Pokemon Trainer
    Define a class Trainer and a class Pokemon. 
    Trainers have:
    Name
    Number of badges
    A collection of pokemon
    Pokemon have:
    Name
    Element
    Health
    All values are mandatory. Every Trainer starts with 0 badges.
    You will be receiving lines until you receive the command "Tournament". Each line will carry information about a pokemon and the trainer who caught it in the format:
    "{trainerName} {pokemonName} {pokemonElement} {pokemonHealth}"
    TrainerName is the name of the Trainer who caught the pokemon. Trainers’ names are unique.
    After receiving the command "Tournament", you will start receiving commands until the "End" command is received. They can contain one of the following:
    "Fire"
    "Water"
    "Electricity"
    For every command you must check if a trainer has at least 1 pokemon with the given element. If he does, he receives 1 badge. Otherwise, all of his pokemon lose 10 health. If a pokemon falls to 0 or less health, he dies and must be deleted from the trainer’s collection. In the end, you should print all of the trainers, sorted by the amount of badges they have in descending order (if two trainers have the same amount of badges, they should be sorted by order of appearance in the input) in the format: 
    "{trainerName} {badges} {numberOfPokemon}"
    Examples
     
    Input	Output	   
    Pesho Charizard Fire 100
    Gosho Squirtle Water 38
    Pesho Pikachu Electricity 10
    Tournament
    Fire
    Electricity
    End	Pesho 2 2
    Gosho 0 1	   
    Stamat Blastoise Water 18
    Nasko Pikachu Electricity 22
    Jicata Kadabra Psychic 90
    Tournament
    Fire
    Electricity
    Fire
    End	Nasko 1 1
    Stamat 0 0
    Jicata 0 1

---
+++ 09-pokemon-trainer/StartUp.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _09_pokemon_trainer
+{
+    class StartUp
+    {
+        public static Dictionary<string, Trainer> trainers = new Dictionary<string, Trainer>();
+        static void Main(string[] args)
+        {
+            //"{trainerName} {pokemonName} {pokemonElement} {pokemonHealth}"
+            string command = "";
+            while((command = Console.ReadLine()) != "Tournament")
+            {
+                var inputArr = command.Split();
+                string trainerName = inputArr[0];
+                string name = inputArr[1];
+                string element = inputArr[2];
+                int health = int.Parse(inputArr[3]);
+                var pokemon = new Pokemon(name, element, health);
+                var trainer = new Trainer(trainerName);
+                if(!trainers.ContainsKey(trainerName))
+                {
+                    trainers.Add(trainerName, new Trainer(trainerName));
+                }
+                    trainers[trainerName].Pokemons.Add(pokemon);
+            }
+            string input = "";
+            while((input = Console.ReadLine()) != "End")
+            {
+                foreach (var trainer in trainers)
+                {
+                    if (trainer.Value.Pokemons.Any(p => p.Element == input))
+                    {
+                        trainer.Value.Badges += 1;   
+                    }
+                    else
+                    {
+                        foreach (var pokemon in trainer.Value.Pokemons)
+                        {
+                           pokemon.Health -= 10;
+                        }
+                        trainers[trainer.Key].RemoveDeadPokemons();
+                    }
+                }
+            }
+            foreach (var trainer in trainers.OrderByDescending(tr => tr.Value.Badges))
+            {
+                Console.WriteLine($"{trainer.Key} {trainer.Value.Badges} {trainer.Value.Pokemons.Count}");
+            }
+        }
+    }
+}
---
+++ 09-pokemon-trainer/Trainer.cs
@@ -0,0 +1,45 @@
+﻿
+namespace _09_pokemon_trainer
+{
+using System;
+using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    public class Trainer
+    {
+        private string name;
+        private int badges;
+        private List<Pokemon> pokemons;
+        public Trainer(string name)
+        {
+            this.Name = name;
+            this.Badges = 0;
+            this.Pokemons = new List<Pokemon>();
+        }
+        
+        //public void ReduceHealth()
+        //{
+
+        //    Pokemons = Pokemons.Select(p => p.Health -= 10);
+        //}
+        public void RemoveDeadPokemons()
+        {
+            Pokemons = Pokemons.Where(p => p.Health > 0).ToList();
+        }
+        public string Name
+        {
+            get { return name; }
+            set { name = value; }
+        }
+        public int Badges
+        {
+            get { return badges; }
+            set { badges = value; }
+        }
+        public List<Pokemon> Pokemons
+        {
+            get { return pokemons; }
+            set { pokemons = value; }
+        }
+    }
+}
