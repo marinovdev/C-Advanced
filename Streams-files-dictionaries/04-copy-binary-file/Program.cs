672d5502f92ca0d385326774c64a244ce0980566
P.M <vyfullspam50@gmail.com>
2019-10-18 21:42:03 +0300

    Add files via upload
    Copy Binary File
    Write a program that copies the contents of a binary file (e.g. image, video, etc.) to another using FileStream. You are not allowed to use the File class or similar helper classes.

---
+++ Streams-files-dictionaries/04-copy-binary-file/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+
+namespace _04_copy_binary_file
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using (FileStream streamReader = File.OpenRead(@"..\..\..\s.jpg"))
+            {
+                using (FileStream streamWriter = File.OpenWrite(@"..\..\..\d.jpg"))
+                {
+                    var binaryReader = new BinaryReader(streamReader);
+                    var binaryWriter = new BinaryWriter(streamWriter);
+
+                    var buffer = new Byte[1024];
+                    int bytesRead = 0;
+
+                    while((bytesRead = streamReader.Read(buffer, 0, 1024)) > 0)
+                    {
+                        streamWriter.Write(buffer, 0, bytesRead);
+                    }
+
+                }
+            }
+        }
+    }
+}
