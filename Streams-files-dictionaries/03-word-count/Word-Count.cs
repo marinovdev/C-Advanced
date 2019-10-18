16c3ea53ffb3d317e6a9129efb07cd6e40a95fb4
P.M <vyfullspam50@gmail.com>
2019-10-18 21:22:01 +0300

    Add files via upload
    Word Count
    Write a program that reads a list of words from the file words.txt and finds how many times each of the words is contained in another file text.txt. Matching should be case-insensitive. Write the results in file actualResults.txt. Sort the words by frequency in descending order and then compare the result with the file expectedResult.txt. Use the File class.
    Examples
     
    words.txt	text.txt	actualResult.txt	expectedResult.txt	   
    quick
    is
    fault	-I was quick to judge him, but it wasn't his fault.
    -Is this some kind of joke?! Is it?
    -Quick, hide here. It is safer.	quick - 2
    is - 3
    fault - 1	is - 3
    quick - 2
    fault - 1

---
+++ Streams-files-dictionaries/03-word-count/Word-Count.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text.RegularExpressions;
+
+namespace _03_word_count
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var expectedDic = new Dictionary<string, int>();
+            var actualDic = new Dictionary<string, int>();
+            var words = new StreamReader(@"..\..\..\words.txt");
+           // var text = new StreamReader(@"..\..\..\text.txt"); // <========
+            var actualResult = new StreamWriter(@"..\..\..\actualResult.txt");
+            var expectedResult = new StreamWriter(@"..\..\..\expectedResult.txt");
+            while(!words.EndOfStream)
+            {
+                var currentWord = words.ReadLine();
+                expectedDic.Add(currentWord, 0);
+                var allText = File.ReadAllText(@"..\..\..\text.txt");
+                int occurances = Regex.Matches(allText.ToLower(), (@"[^\w]" + currentWord.ToLower()) + @"[^\w]").Count;
+                expectedDic[currentWord] = occurances;
+            }
+            foreach (var word in expectedDic)
+            {
+                expectedResult.WriteLine(word.Key + " - " + word.Value);
+            }
+            actualDic = expectedDic.OrderByDescending(x => x.Value).ToDictionary(x => x.Key, x => x.Value);
+            foreach (var word in actualDic)
+            {
+                actualResult.WriteLine(word.Key + " - " + word.Value);
+            }
+            expectedResult.Close();
+            actualResult.Close();
+
+        }
+    }
+}
