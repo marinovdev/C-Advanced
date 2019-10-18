adab5cc2d27e22b65909d327f2886347cc8849b6
P.M <vyfullspam50@gmail.com>
2019-10-18 13:38:37 +0300

    Add files via upload
    Even Lines
    Write a program that reads a text file and prints on the console its even lines. Line numbers start from 0. Use StreamReader. Before you print the result replace {"-", ",", ".", "!", "?"} with "@" and reverse the order of the words.
    Examples
     
    text.txt	output	   
    -I was quick to judge him, but it wasn't his fault.
    -Is this some kind of joke?! Is it?
    -Quick, hide here. It is safer.	fault@ his wasn't it but him@ judge to quick was @I
    safer@ is It here@ hide @Quick@

---
+++ Streams-files-dictionaries/01-even-numbers/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+namespace _01_even_lines
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string fileName = @"..\..\..\text.txt";
+            string writerFile = @"..\..\..\output.txt";
+            int counter = 0;
+            var letterChar = new char[] { '-', ',', '.', '!', '?' };
+            using (StreamReader reader = new StreamReader(fileName))
+            {
+                using (StreamWriter writer = new StreamWriter(writerFile))
+                {
+                    while (!reader.EndOfStream)
+                    {
+                        var readLine = reader.ReadLine()
+                                .Split().ToList();
+                        readLine.Reverse();
+
+                        if (counter % 2 == 0)
+                        {
+                            for (int i = 0; i < readLine.Count; i++)
+                            {
+                                var word = readLine[i];
+                                foreach (var symbol in letterChar)
+                                {
+                                    word = word.Replace(symbol, '@');
+                                }
+                                readLine[i] = word;
+                            }
+                            string output = string.Join(" ", readLine);
+                            writer.WriteLine(output);
+                            readLine.Clear();
+                        }
+                        counter++;
+                    }
+                }
+            }
+        }
+
+    }
+}
