82fe66739b6d7ead14deb58922e69ac2976875b0
P.M <vyfullspam50@gmail.com>
2019-10-19 16:45:53 +0300

    Add files via upload
    Generic Box of String
    Create a generic class Box that can be initialized with any type and store the value. Override the ToString() method and print the type and stored value.
    
    Input
    On the first line, you will get n - the number of strings to read from the console.
    On the next n lines, you will get the actual strings.
    For each of them, create a box and call its ToString() method to print its data on the console.
    Output
    The output should be in the given format:
    
    "{class full name: value}"
    Examples
     
    Input	Output	   
    2
    life in a box
    box in a life	System.String: life in a box
    System.String: box in a life

---
+++ Generic/01-generic-box-of-string/StartUp.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _01_generic_box_of_string
+{
+    class Program
+    {
+        public static List<string> list = new List<string>();
+        static void Main(string[] args)
+        {
+            var n = int.Parse(Console.ReadLine());
+            for (int i = 0; i < n; i++)
+            {
+                var input = Console.ReadLine();
+                list.Add(input);
+            }
+            for (int i = 0; i < list.Count; i++)
+            {
+                var read = list[i];
+                var box = new Box<string>(read);
+                box.ToString();
+            }
+        }
+    }
+
+    class Box<T>
+        where T: class
+    {
+        public T Value { get; set; }
+
+        public Box(T text)
+        {
+            this.Value = text;
+        }
+
+        public void ToString()
+        {
+            Console.WriteLine($"{Value.GetType()}: {Value}");
+        }
+    }
+}
