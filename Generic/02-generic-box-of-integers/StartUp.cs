3d04fd4732f82b6bcda181069b8578a20e3ff9df
P.M <vyfullspam50@gmail.com>
2019-10-19 16:56:07 +0300

    Add files via upload
    Generic Box of Integer
    Use the description of the previous problem but now, test your generic box with Integers.
    Examples
     
    Input	Output	   
    3
    7
    123
    42	System.Int32: 7
    System.Int32: 123
    System.Int32: 42

---
+++ Generic/02-generic-box-of-integers/StartUp.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _01_generic_box_of_string
+{
+    class Program
+    {
+        public static List<int> list = new List<int>();
+        static void Main(string[] args)
+        {
+            var n = int.Parse(Console.ReadLine());
+            for (int i = 0; i < n; i++)
+            {
+                var input = int.Parse(Console.ReadLine());
+                list.Add(input);
+            }
+            for (int i = 0; i < list.Count; i++)
+            {
+                var read = list[i];
+                var box = new Box<int>(read);
+                box.ToString();
+            }
+        }
+    }
+
+    class Box<T>
+        where T : struct
+    {
+        public T Value { get; set; }
+
+        public Box(T number)
+        {
+            this.Value = number;
+        }
+
+        public void ToString()
+        {
+            Console.WriteLine($"{Value.GetType()}: {Value}");
+        }
+    }
+}
