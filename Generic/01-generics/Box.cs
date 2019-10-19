064dc05e8de90294079dcaacd39a76caa572fcc6
P.M <vyfullspam50@gmail.com>
2019-10-19 13:29:45 +0300

    Add files via upload
    Part I: Generics
    Box of T
    NOTE: You need a public StartUp class with the namespace BoxOfT.
    Create a class Box<> that can store anything. It should have two public methods:
    void Add(element) – adds an element on the top of the list.
    element Remove() – removes the topmost element.
    int Count { get; }
    Examples
     
    public static void Main(string[] args)
    {
        Box<int> box = new Box<int>();
        box.Add(1);
        box.Add(2);
        box.Add(3);
        Console.WriteLine(box.Remove());
        box.Add(4);
        box.Add(5);
        Console.WriteLine(box.Remove());
    }	 
    Hints
    Use the syntax Box<T> to create a generic class

---
+++ Generic/01-generics/Box.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace _01_generics
+{
+    class Box<T>
+    {
+        private static List<T> collection;
+
+        public Box()
+        {
+            Collection = new List<T>();
+        }
+        public int Count
+        {
+            get
+            {
+                return Collection.Count();
+            }
+
+        }
+        public static List<T> Collection
+        {
+            get { return collection; }
+            set { collection = value; }
+        }
+
+        public void Add(T element)
+        {
+            Collection.Add(element);
+        }
+
+        public T Remove()
+        {
+            T var = Collection.ElementAt(0);
+            Collection.RemoveAt(0);
+            return var;
+        }
+
+    }
+}
