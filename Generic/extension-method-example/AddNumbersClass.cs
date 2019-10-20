ad306cc07b1abe504263d3e2f076d58222bcef8f
P.M <vyfullspam50@gmail.com>
2019-10-20 17:06:12 +0300

    Add files via upload

---
+++ Generic/extension-method-example/AddNumbersClass.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace extension_method_example
+{
+    public static class AddNumbersClass
+    {
+        // extension method :
+        public static string AddNumbers(this string value)
+        {
+            return value += "123";
+        }
+    }
+}
---
+++ Generic/extension-method-example/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace extension_method_example
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var box = new Box<string>();
+            box.Values = "Random text";
+            Console.WriteLine(box.ToString());
+        }
+    }
+
+    class Box<T>
+    {
+        private T values;
+
+        public Box()
+        {
+            this.values = default(T);
+        }
+
+        public T Values
+        {
+            get { return values; }
+            set { values = value; }
+        }
+
+        public override string ToString()
+        {
+            //extension methods are attatched as suffix to a given var :
+            return $"{Values.GetType()}: {Values}".AddNumbers();
+        }
+
+    }
+}
