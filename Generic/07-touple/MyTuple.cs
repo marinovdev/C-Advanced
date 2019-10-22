148449da02b5755625ca1a3b4d85a580effd1dd3
P.M <vyfullspam50@gmail.com>
2019-10-22 18:41:27 +0300

    Add files via upload
    Tuple
    A Tuple is a class in C#, in which you can store a few objects. First, we are going to focus on the Tuple's type, which contains two objects. The first one is "item1" and the second one is "item2". It is kind of like a KeyValuePair, except – it simply has items, which are neither key nor value. Your task is to create a class "Tuple", which holds two objects. The first one, will be "item1" and the second one – "item2". The tricky part here is to make the class hold generics. This means, that when you create a new object of class – "Tuple", there should be a way to explicitly specify both items' type separately.
    Input
    The input consists of three lines:
    The first one is holding a person's name and an address. They are separated by space(s). Your task is to collect them in the tuple and print them on the console. Format of the input:
    {first name} {last name} {address}
    The second line holds a name of a person and the amount of beer (int) he can drink. Format:
    {name} {liters of beer}
    The last line will hold an integer and a double. Format:
    {integer} {double}
    Output
    Print the tuples’ items in format: {item1} -> {item2}
    Constraints
    Use the good practices we have learned. Create the class and make it have getters and setters for its class variables. The input will be valid, no need to check it explicitly!
    Examples
     
    Input	Output	   
    Adam Smith California
    Mark 2
    23 21.23212321	Adam Smith -> California
    Mark -> 2
    -> 21.23212321

---
+++ Generic/07-touple/MyTuple.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace _07_touple
+{
+    class MyTuple<T, TT>
+    {
+        private T item1;
+        private TT item2;
+
+        public MyTuple(T item1, TT item2)
+        {
+            this.Item1 = item1;
+            this.Item2 = item2;
+        }
+
+        public override string ToString()
+        {
+            return $"{this.Item1} -> {this.Item2}";
+        }
+
+        public TT Item2
+        {
+            get { return item2; }
+            set { item2 = value; }
+        }
+
+        public T Item1
+        {
+            get { return item1; }
+            set { item1 = value; }
+        }
+
+    }
+}
---
+++ Generic/07-touple/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Runtime.CompilerServices;
+
+namespace _07_touple
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+         
+            var input1 = Console.ReadLine().Split(' ', StringSplitOptions.RemoveEmptyEntries);
+            var name = input1[0] + " " + input1[1];
+            var address = input1[2];
+            var input2 = Console.ReadLine().Split();
+            var name2 = input2[0];
+            var littres = int.Parse(input2[1]);
+            var input3 = Console.ReadLine().Split();
+            var intNum = int.Parse(input3[0]);
+            var doubleNum = double.Parse(input3[1]);
+            var personInfo = new MyTuple<string, string>(name, address);
+            var beerInfo = new MyTuple<string, int>(name2, littres);
+            var numbersInfo = new MyTuple<int, double>(intNum, doubleNum);
+            Console.WriteLine($"{personInfo.ToString()}");
+            Console.WriteLine($"{beerInfo}");
+            Console.WriteLine($"{numbersInfo}");
+        }
+    }
+}
