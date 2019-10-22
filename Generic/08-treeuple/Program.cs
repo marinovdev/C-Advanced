afaaf2fbce8d7189cd3a01cff56b316af0dcfc21
P.M <vyfullspam50@gmail.com>
2019-10-22 18:59:31 +0300

    Add files via upload
    Threeuple
    Create a Class Threeuple. Its name is telling us, that it will hold no longer, just a pair of objects. The task is simple, our Threeuple should hold three objects. Make it have getters and setters. You can even extend the previous class
    Input
    The input consists of three lines:
    The first one is holding a name, an address and a town. Format of the input:
    {first name} {last name} {address} {town}
    The second line is holding a name, beer liters, and a boolean variable with value - drunk or not. Format:
    {name} {liters of beer} {drunk or not}
    The last line will hold a name, a bank balance (double) and a bank name. Format:
    {name} {account balance} {bank name}
    Output
    Print the Threeuples' objects in format:
    "{firstElement} -> {secondElement} -> {thirdElement}"
    Examples
     
    Input	Output	   
    Adam Smith Wallstreet New York
    Mark 18 drunk
    Karren 0.10 USBank	Adam Smith -> Wallstreet -> New York
    Mark -> 18 -> True
    Karren -> 0.1 -> USBank	   
    Ivan Ivanov TheHills Plovdiv
    Mitko 18 not
    George 0.10 NGB	Ivan Ivanov -> TheHills -> Plovdiv
    Mitko -> 18 -> False
    George -> 0.1 -> NGB

---
+++ Generic/08-treeuple/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace treeuple
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var input1 = Console.ReadLine().Split(' ', StringSplitOptions.RemoveEmptyEntries);
+            var name = input1[0];
+            var lastName = " " + input1[1];
+            var address = input1[2];
+            var town = input1[3];
+            if(input1.Length > 4)
+            {
+                town += " " + input1[4];
+            }
+
+            var input2 = Console.ReadLine().Split();
+            var name2 = input2[0];
+            var littres = int.Parse(input2[1]);
+            var condition = false;
+            if (input2[2] == "drunk") condition = true;
+            else condition = false;
+
+            var input3 = Console.ReadLine().Split();
+            var nameBankUser = input3[0];
+            var doubleNum = double.Parse(input3[1]);
+            var bankName = input3[2];
+
+            var personInfo = new Treeuple<string, string, string>(name + lastName, address, town);
+            var beerInfo = new Treeuple<string, int, bool>(name2, littres, condition);
+            var numbersInfo = new Treeuple<string, double, string>(nameBankUser, doubleNum, bankName);
+            Console.WriteLine($"{personInfo}");
+            Console.WriteLine($"{beerInfo}");
+            Console.WriteLine($"{numbersInfo}");
+        }
+    }
+}
---
+++ Generic/08-treeuple/Treeuple.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace treeuple
+{
+    class Treeuple<T, TT, TTT>
+    {
+        private T item1;
+        private TT item2;
+        private TTT item3;
+
+
+        public Treeuple(T item1, TT item2, TTT item3)
+        {
+            this.Item1 = item1;
+            this.Item2 = item2;
+            this.Item3 = item3;
+        }
+
+        public override string ToString()
+        {
+            return $"{this.Item1} -> {this.Item2} -> {this.Item3}";
+        }
+        public TTT Item3
+        {
+            get { return item3; }
+            set { item3 = value; }
+        }
+
+        public TT Item2
+        {
+            get { return item2; }
+            set { item2 = value; }
+        }
+
+        public T Item1
+        {
+            get { return item1; }
+            set { item1 = value; }
+        }
+
+    }
+}
