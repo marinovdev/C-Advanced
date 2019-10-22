1747930ada56f39aa92baf4dc8cb860a04106b80
P.M <vyfullspam50@gmail.com>
2019-10-22 18:42:15 +0300

    Add files via upload
    Generic Count Method Strings
    Create a method that receives as an argument a list of any type, that can be compared and an element of the given type. The method should return the count of elements that are greater than the value of the given element. Modify your Box class to support comparison by value of the stored data.
    
    Input
     On the first line, you will receive n - the number of elements to add to the list.
     On the next n lines, you will receive the actual elements.
     On the last line, you will get the value of the element for comparison.
    Output
    Print the count of elements that are larger than the value of the given element.
    Examples
     
    Input	Output	   
    3
    aa
    aaa
    bb
    aa	2	 
    Generic Count Method Doubles
    Use the description of the previous problem, but now, test your list of generic boxes with doubles.
    Examples
     
    Input	Output	   
    3
    7.13
    123.22
    42.78
    7.55	2

---
+++ Generic/06-generic-count-method-doubles/GenericCount.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _05_generic_count_method_strings
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var n = int.Parse(Console.ReadLine());
+            var list = new Box<double>();
+            for (int i = 0; i < n; i++)
+            {
+                list.Values.Add(double.Parse(Console.ReadLine()));
+            }
+
+            var comparisonValue = double.Parse(Console.ReadLine());
+            Console.WriteLine(list.GraterValues(comparisonValue));
+
+        }
+    }
+
+    class Box<T>
+        where T : IComparable
+    {
+        private List<T> values;
+
+        public Box()
+        {
+            this.Values = new List<T>();
+        }
+        public List<T> Values
+        {
+            get { return values; }
+            set { values = value; }
+        }
+
+        public int GraterValues(T targtItem)
+        {
+            int counter = 0;
+            foreach (var item in Values)
+            {
+                if (item.CompareTo(targtItem) > 0)
+                {
+                    counter++;
+                }
+            }
+            return counter;
+        }
+    }
+}
